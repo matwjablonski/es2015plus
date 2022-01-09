ab0f568ca2140729e87818b9c8199da2877e08f8
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-01-09 21:20:44 +0100

    excercise

---
+++ exercise.js
@@ -0,0 +1,26 @@
+const students = [
+    [ 'Kamil', 30 ],
+    [ 'Ola', 23 ],
+    [ 'Krzysztof', 21 ],
+    [ 'Marek', 54 ],
+    [ 'Alicja', 31 ],
+];
+
+// ex 1
+// zmapuj tablicę tablic (pierwszy element to imię, drugi to wiek) na tablicę obiektów { name, age }
+
+
+// ex 2
+// odfiltruj wszystkich studentów mających mniej niż 30 lat
+
+
+// ex 3
+// zsumuj wiek wszystkich studentów (reduce)
+
+
+// ex 4
+// czy jest przynajmniej jeden student starszy niż 40 lat
+
+
+// dodatkowe zadanie
+// posortuj studentów wg wieku (sort)
